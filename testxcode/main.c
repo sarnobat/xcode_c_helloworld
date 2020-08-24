4145ea4fb248fa86141804126c3b9d6e1e202f2c
Sridhar Sarnobat <ss401533@gmail.com>
2020-08-23 17:48:31 -0700

    Initial Commit

---
+++ testxcode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  testxcode
+//
+//  Created by Sridhar Sarnobat on 8/23/20.
+//  Copyright © 2020 Sridhar Sarnobat. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
