98a6645401cefd8e27126bd84e568746524c6a89
Sridhar Sarnobat <ss401533@gmail.com>
2020-08-23 18:13:19 -0700

    Minor changes (untested)

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
 xcodebuild
+# Output gets generated in build/
+#
+# If you build inside XCode, the output will be generated in a different place:
+# 
+# 	/Users/sarnobat/Library/Developer/Xcode/DerivedData/testxcode-cjfaaanpsuuuhbfnedczzadgfnnn/Build/Products/Debug/testxcode
